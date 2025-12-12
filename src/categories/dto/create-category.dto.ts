d5ef7e521860190c9531c244cb4707f3faa0e895
Angel8118 <angel8118campos@gmail.com>
2025-12-12 13:38:57 -0600

    Add validation for create-DTO

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,7 @@
-export class CreateCategoryDto {}
+import { IsString, IsNotEmpty } from "class-validator";
+    
+export class CreateCategoryDto {
+    @IsString({ message: 'Name must be a string' })
+    @IsNotEmpty({ message: 'Name must not be empty' })
+    name: string;
+}
