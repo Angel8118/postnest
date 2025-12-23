a794b5a2194c57486d3fa40070046036cd4eb7d7
Angel8118 <angel8118campos@gmail.com>
2025-12-23 11:44:46 -0600

    create transaction resourse and sales diagram

---
+++ src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Transaction {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column('decimal')
+    total: number;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP(6)' })
+    transactionDate: Date;
+}
