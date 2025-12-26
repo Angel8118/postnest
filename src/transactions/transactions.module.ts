ed2180f6ca529b89bbd8d5bf5ea4e2597afbe3cc
Angel8118 <angel8118campos@gmail.com>
2025-12-26 13:48:45 -0600

    add repositories and save sales

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
+import { Transaction, TransactionContents } from './entities/transaction.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Transaction, TransactionContents])],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction, TransactionContents } from './entities/transaction.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    @InjectRepository(Transaction) private readonly transactionsRepository: Repository<Transaction>,
+    @InjectRepository(TransactionContents) private readonly transactionContentsRepository: Repository<TransactionContents>,
+  ) {}
+
+
+  async create(createTransactionDto: CreateTransactionDto) {
+    const transaction = new Transaction();
+    transaction.total = createTransactionDto.total;
+    await this.transactionsRepository.save(transaction);
+    for (const contents of createTransactionDto.contents) {
+      await this.transactionContentsRepository.save({
+        ...contents,
+        transaction
+      })
+    }
+    return "Sale saved Successful";
   }
 
   findAll() {
