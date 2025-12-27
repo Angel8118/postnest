7228ce8f4395419fa13999f7f382e340eb007039
Angel8118 <angel8118campos@gmail.com>
2025-12-27 17:27:08 -0600

    feat(Homepage Store): validate response API with zod schema

--- frontend/app/(store)/[categoryId]/page.tsx
+++ frontend/app/(store)/[categoryId]/page.tsx
@@ -1,10 +1,13 @@
+import { CategoryWithProductsResponseSchema } from "@/app/src/schemas";
+
 type Params = Promise<{ categoryId: string }>
 
 async function getProducts(categoryId:string) {
     const url = `${process.env.API_URL}/categories/${categoryId}?products=true`;
     const req = await fetch(url, {cache: 'no-store'});
     const json = await req.json();
-    return json;
+    const products = CategoryWithProductsResponseSchema.parse(json);
+    return products;
 }
 
 export default async function StorePage({ params }: { params: Params }) {
