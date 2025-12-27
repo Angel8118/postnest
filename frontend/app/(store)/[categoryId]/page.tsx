88829aff8f4d9deb42c8e07bceeac87e11563f8e
Angel8118 <angel8118campos@gmail.com>
2025-12-27 17:03:30 -0600

    feat(Homepage Store): get actual category

--- frontend/app/(store)/[categoryId]/page.tsx
+++ frontend/app/(store)/[categoryId]/page.tsx
@@ -1,5 +1,9 @@
-export default function StorePage() {
-  return (
+
+type Params = Promise<{ categoryId: string }>
+
+export default async function StorePage({ params }: { params: Params }) {
+    const { categoryId } = await params
+    return (
     <div>StorePage</div>
   )
 }
