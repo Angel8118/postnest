11c625559a2e140383395118bd69e6167b2203b4
Angel8118 <angel8118campos@gmail.com>
2025-12-27 11:28:44 -0600

    add validations

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -1,1 +1,18 @@
-export class CreateCouponDto {}
+import { IsDateString, IsInt, IsNotEmpty, Max, Min } from "class-validator";
+
+export class CreateCouponDto {
+
+    @IsNotEmpty({ message: 'Name should not be empty' })   
+    name: string;
+
+    @IsNotEmpty({ message: 'Percentage should not be empty' })
+    @IsInt({ message: 'Percentage must be between 1 and 100' })
+    @Max(100, { message: 'Percentage must be between 1 and 100' })
+    @Min(1, { message: 'Percentage must be between 1 and 100' })
+    percentage: number;
+
+    @IsNotEmpty({ message: 'Expiration date should not be empty' })
+    @IsDateString({}, { message: 'Expiration date must be a valid date string' })
+    expirationDate: Date;
+
+}
