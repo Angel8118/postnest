13d2af49926f1b9b276913a1a8240dbe835c3e21
Angel8118 <angel8118campos@gmail.com>
2025-12-26 22:49:58 -0600

    find all transactions or sales

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -3,7 +3,7 @@ import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Transaction, TransactionContents } from './entities/transaction.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
@@ -60,7 +60,12 @@ export class TransactionsService {
   }
 
   findAll() {
-    return `This action returns all transactions`;
+    const options : FindManyOptions<Transaction> = {
+      relations: {
+        contents: true
+      }
+    }
+    return this.transactionsRepository.find(options);
   }
 
   findOne(id: number) {
