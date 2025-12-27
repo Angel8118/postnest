05bbe0fbf9ea27caeceeb9a43c91dd45863c36f1
Angel8118 <angel8118campos@gmail.com>
2025-12-27 17:18:25 -0600

    feat(Homepage Store): callind API by category

--- frontend/app/(store)/[categoryId]/page.tsx
+++ frontend/app/(store)/[categoryId]/page.tsx
@@ -1,8 +1,15 @@
-
 type Params = Promise<{ categoryId: string }>
+
+async function getProducts(categoryId:string) {
+    const url = `${process.env.API_URL}/categories/${categoryId}?products=true`;
+    const req = await fetch(url, {cache: 'no-store'});
+    const json = await req.json();
+    return json;
+}
 
 export default async function StorePage({ params }: { params: Params }) {
     const { categoryId } = await params
+    const products =await getProducts(categoryId);
     return (
     <div>StorePage</div>
   )
