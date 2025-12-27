a22b804ce236b24480f407c49bd7472f2bb81853
Angel8118 <angel8118campos@gmail.com>
2025-12-27 13:32:07 -0600

    clear database and add images

--- src/seeder.ts
+++ src/seeder.ts
@@ -5,6 +5,7 @@ import { SeederService } from './seeder/seeder.service';
 async function bootstrap() {
     const app = await NestFactory.create(SeederModule);
     const seeder = app.get(SeederService);
+    await seeder.onModuleInit();
     await seeder.seed();
     await app.close();
 }
