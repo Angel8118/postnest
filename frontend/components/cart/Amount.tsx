1269e77ec1fb311de1fea561915320605b7bc61d
Angel8118 <angel8118campos@gmail.com>
2025-12-30 12:04:27 -0600

    feat(Apply Discount Coupon): show discount

--- frontend/components/cart/Amount.tsx
+++ frontend/components/cart/Amount.tsx
@@ -3,11 +3,12 @@ import { formatCurrency } from "@/src/utils";
 type AmountProps = {
     label: string;
     amount: number;
+    discount?: boolean;
 }
 
-export default function Amount({ label, amount }: AmountProps) {
+export default function Amount({ label, amount, discount }: AmountProps) {
   return (
-    <div className="flex justify-between">
+    <div className={`${discount ? "bg-green-400 text-green-900" : "text-gray-900"} flex justify-between p-1`}>
         <dt className="font-bold">
             {label}
         </dt>
