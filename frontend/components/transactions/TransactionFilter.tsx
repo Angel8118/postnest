448ec780a7492fece1ac363cde221a7464dda437
Angel8118 <angel8118campos@gmail.com>
2025-12-31 11:25:30 -0600

    feat(Admin Panel - List Sales): add calendar to filter by date

--- frontend/components/transactions/TransactionFilter.tsx
+++ frontend/components/transactions/TransactionFilter.tsx
@@ -1,16 +1,27 @@
 "use client"
 
+import { useState } from "react";
 import Calendar from "react-calendar"
+import 'react-calendar/dist/Calendar.css';
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function TransactionFilter() {
+
+    const [date, setDate] = useState<Value>(new Date());
+
   return (
-    <div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10">
         <div>
-            <Calendar />
+            <Calendar 
+            value={date}
+            onChange={setDate}
+            />
         </div>
 
         <div>
-
+2
         </div>
     </div>
   )
