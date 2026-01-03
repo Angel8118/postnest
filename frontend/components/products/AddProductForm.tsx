6b46e5ac2c12955ee9d71c081bd241dfe9150c93
Angel8118 <angel8118campos@gmail.com>
2026-01-03 12:11:25 -0600

    feat(Admin Panel - CRUD Products): add form to create products

--- frontend/components/products/AddProductForm.tsx
+++ frontend/components/products/AddProductForm.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-export default function AddProductForm() {
+import ProductForm from "./ProductForm"
+
+export default function AddProductForm({children}: {children?: React.ReactNode}) {
   return (
-    <form action="">
-
-
+    <form className="space-y-5">
+        {children}
         <input type="submit"
         className="rounded bg-green-400 font-bold py-2 w-full cursor-pointer"
         value="Agregar Producto" />
