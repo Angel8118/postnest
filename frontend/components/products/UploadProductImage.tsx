6d2a5cecd40460a6804df0a4de911ba260cdb3c0
Angel8118 <angel8118campos@gmail.com>
2026-01-04 10:22:27 -0600

    feat(Admin Panel - Update Images): show img uploaded on the form

--- frontend/components/products/UploadProductImage.tsx
+++ frontend/components/products/UploadProductImage.tsx
@@ -1,20 +1,24 @@
 "use client"
 
 import { uploadImage } from "@/actions/upload-image-action";
-import { useCallback } from "react"
+import Image from "next/image";
+import { use, useCallback, useState } from "react"
 import { useDropzone } from "react-dropzone"
-
-const onDrop = useCallback(async (files : File[]) => {
-    const formData = new FormData();
-    files.forEach((file) => {
-        formData.append('file', file);
-    })
-    const image = await uploadImage(formData);
-}, [])
 
 export default function UploadProductImage() {
 
-    const {getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject} = useDropzone({
+    const onDrop = useCallback(async (files: File[]) => {
+        const formData = new FormData();
+        files.forEach((file) => {
+            formData.append('file', file);
+        })
+        const image = await uploadImage(formData);
+        setImage(image);
+    }, [])
+
+    const [image, setImage] = useState('');
+
+    const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
         accept: {
             'image/jpeg': ['.jpg', '.jpeg'],
             'image/png': ['.png']
@@ -23,24 +27,38 @@ export default function UploadProductImage() {
         maxFiles: 1
     })
 
-  return (
-    <>
-      <div className="space-y-1">
-    <label className="block text-sm font-medium leading-6 text-gray-900">
-      Imagen Producto
-    </label>
-    <div {...getRootProps({
-      className: `
+    return (
+        <>
+            <div className="space-y-1">
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                    Imagen Producto
+                </label>
+                <div {...getRootProps({
+                    className: `
             py-20 border-2 border-dashed  text-center 
             ${isDragActive ? 'border-gray-900 text-gray-900 bg-gray-200 ' : 'border-gray-400 text-gray-400 bg-white'} 
             ${isDragReject ? 'border-none bg-white' : 'cursor-not-allowed'}
         `})}>
-      <input {...getInputProps()} />
-      {isDragAccept && (<p>Suelta la Imagen</p>)}
-      {isDragReject && (<p>Archivo no válido</p>)}
-      {!isDragActive && (<p>Arrastra y suelta una imagen aquí</p>)}
-    </div>
-  </div>
-    </>
-  )
+                    <input {...getInputProps()} />
+                    {isDragAccept && (<p>Suelta la Imagen</p>)}
+                    {isDragReject && (<p>Archivo no válido</p>)}
+                    {!isDragActive && (<p>Arrastra y suelta una imagen aquí</p>)}
+                </div>
+            </div>
+            {image && (
+                <div className="py-5 space-y-3">
+                    <p className="font-bold">Imagen Producto:</p>
+                    <div className="w-[300px] h-[420px] relative">
+                        <Image
+                            src={image}
+                            alt="Product Image"
+                            fill
+                            className="object-cover"
+                        />
+                    </div>
+                </div>
+            )}
+            <input type="hidden" name="image" defaultValue={image} />
+        </>
+    )
 }
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+    ],
+  },
   async rewrites() {
-    return [
-      {
-        source: "/img/:path*",
-        destination: "http://localhost:3000/img/:path*",
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/img/:path*",
+          destination: "http://localhost:3000/img/:path*",
+        },
+      ],
+    };
   },
 };
 
